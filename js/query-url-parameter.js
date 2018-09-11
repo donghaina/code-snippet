30394454e669073603a1bed1e3d1f7d9c0c7352a
taosang1992 <taosang1992@gmail.com>
2018-09-12 07:59:58 +0800

    update get query args

--- js/query-url-parameter.js
+++ js/query-url-parameter.js
@@ -1,8 +1,12 @@
 function queryURLParameter(url){
+	var query_obj = {};
 	var index = url.indexOf('?');
+	if(index===-1){
+		return query_obj;
+	}
 	var query = url.slice(index+1);
 	var query_array = query.split('&');
-	var query_obj = {};
+	
 	query_array.forEach(item=>{
 		item_index = item.indexOf('=');
 		query_obj[item.slice(0,item_index)] = item.slice(item_index+1);
@@ -10,6 +14,6 @@ function queryURLParameter(url){
 	return query_obj;
 }
 
-var url = 'https://www.google.com/search?q=hello+world&oq=hello+world&aqs=chrome..69i57.3433j0j1&sourceid=chrome&ie=UTF-8';
+var url = 'https://www.google.com/search';
 var query_args = queryURLParameter(url);
 console.log(query_args);
